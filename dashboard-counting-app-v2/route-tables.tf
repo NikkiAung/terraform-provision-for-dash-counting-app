a664fd5201420dd392fde293abee3aea30256bcb
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2026-03-22 15:22:06 -0700

    impls route tables

---
+++ dashboard-counting-app-v2/route-tables.tf
@@ -0,0 +1,49 @@
+# ─────────────────────────────────────────
+# PUBLIC ROUTE TABLE
+# Used by: public subnet (bastion)
+# Rule: send internet traffic → IGW
+# ─────────────────────────────────────────
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+# Associate public route table with public subnet
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# ─────────────────────────────────────────
+# PRIVATE ROUTE TABLE
+# Used by: private subnet (app server)
+# Rule: send internet traffic → NAT Gateway
+# ─────────────────────────────────────────
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
+}
+
+# Associate private route table with private subnet
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
